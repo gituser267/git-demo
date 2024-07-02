47289a31761afd806a173a625752f5e231bac536
gtuser267 <psmitra2019@gmail.com>
2024-07-02 13:07:38 +0530

    feature2 added

--- source.c
+++ source.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
 
 void feature1(void);
+void feature2(void);
+
+
 
 int main(void){
     printf("hello world\n");
@@ -15,4 +18,8 @@ void feature1(void){
     printf("new line in feature1\n");
     printf("next line\n");
 
+}
+void feature2(void){
+    printf("this is feature2\n");
+    printf("welocme to feature2"\n);
 }
